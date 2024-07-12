3f39dd182b237e6f000f744af416eaff7396ee69
Godfrey Onyango <godiodhis268@gmail.com>
2024-07-12 19:52:50 +0300

    last

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded',function(){
+  const fom =document.getElementById("create-task-form");
+  const textArea=document.getElementById("new-task-description");
+  const clear=document.getElementById('clearing');
+  
+  
+  const cont=document.getElementById('tasks');
+  fom.addEventListener('submit',function(e){
+    e.preventDefault();
+    const item=document.createElement("li");
+    item.textContent=textArea.value
+    
+    cont.appendChild(item);
+    
+    textArea.value="";
+
+    const btn=document.createElement('button');
+    btn.innerText="x";
+    btn.addEventListener('click',function(){
+      item.remove();
+    });
+    item.appendChild(btn);
+
+    
+    ;
+    clear.addEventListener('click',function(){
+      cont.innerHTML='';
+      
+
+    });
+    
+      
+    
+    
+
+    
+  });
+  
 });
+
+
+  
